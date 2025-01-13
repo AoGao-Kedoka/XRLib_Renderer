45f0052a708e319244ccc966696ad5a8a76e632a
Ao Gao <ao.gao@tum.de>
2025-01-13 15:24:47 +0100

    FIX: Fix gcc compilation error from assimp by version update

--- cmake/assimp.cmake
+++ cmake/assimp.cmake
@@ -4,7 +4,7 @@ if (NOT assimp_FOUND)
 	message("${MESSAGE_BOX}\nAssimp not found, try fetching\n${MESSAGE_BOX}")
 	FetchContent_Declare(assimp
 		GIT_REPOSITORY https://github.com/assimp/assimp.git
-		GIT_TAG v5.4.1)
+		GIT_TAG v5.4.3)
 	set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
 	set(ASSIMP_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 	set(ASSIMP_INJECT_DEBUG_POSTFIX OFF CACHE BOOL "" FORCE)
